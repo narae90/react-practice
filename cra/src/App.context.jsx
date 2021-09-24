3d0edc28d3325950e6b1c116ddca05a4e890894d
김나래 <narae900@naver.com>
2021-09-24 13:46:02 +0900

    Context API

---
+++ cra/src/App.context.jsx
@@ -0,0 +1,13 @@
+import ContextApi from './components/ContextApi';
+
+function App() {
+    return(
+        <div>
+            <h2>Using Context API</h2>
+            <ContextApi />
+        </div>
+    )
+}
+
+
+export default App;
---
+++ cra/src/components/ContextChildren.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Children2 from './ContextChildren2';
+
+class ContextChildren extends React.Component{
+    render(){
+        return(
+            <Children2 />
+        )
+    }
+}
+
+export default ContextChildren;
